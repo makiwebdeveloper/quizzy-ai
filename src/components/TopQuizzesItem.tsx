cf8a4d7726b97cd078a30ec6f13101d72a6895a1
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-08-22 21:04:31 +0200

    redirect from top quizzes

--- src/components/TopQuizzesItem.tsx
+++ src/components/TopQuizzesItem.tsx
@@ -1,33 +1,38 @@
+"use client";
+
 import { IQuiz } from "@/types/quiz.interface";
 import { Card, CardHeader, CardTitle } from "./ui/Card";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface Props {
   quiz: IQuiz;
 }
 
 export default function TopQuizzesItem({ quiz }: Props) {
+  const router = useRouter();
+
   return (
-    <Link href={`/play/${quiz.id}`}>
-      <Card
-        key={quiz.id}
-        className="flex flex-col md:flex-row md:items-center justify-between"
-      >
-        <CardHeader>
-          <CardTitle className="flex items-center">
-            Topic:
-            <span className="text-base ml-2 py-1 px-2 rounded-lg bg-primary text-primary-foreground">
-              {quiz.topic}
-            </span>
-          </CardTitle>
-        </CardHeader>
-        <p className="font-semibold px-6 pb-6 md:pb-0">
-          Questions length:
-          <span className="ml-2 py-1 p-2 rounded-lg bg-primary text-primary-foreground">
-            {quiz.questions.length}
+    <Card
+      key={quiz.id}
+      className="flex cursor-pointer hover:bg-accent flex-col md:flex-row md:items-center justify-between"
+      onClick={() => {
+        router.push(`/play/${quiz.id}`);
+      }}
+    >
+      <CardHeader>
+        <CardTitle className="flex items-center">
+          Topic:
+          <span className="text-base ml-2 py-1 px-2 rounded-lg bg-primary text-primary-foreground">
+            {quiz.topic}
           </span>
-        </p>
-      </Card>
-    </Link>
+        </CardTitle>
+      </CardHeader>
+      <p className="font-semibold px-6 pb-6 md:pb-0">
+        Questions length:
+        <span className="ml-2 py-1 p-2 rounded-lg bg-primary text-primary-foreground">
+          {quiz.questions.length}
+        </span>
+      </p>
+    </Card>
   );
 }
